3ba3d2d4e34b95d71d5aa2a74d4aca515ff8f4b0
zr99lm <z1064207498@gmail.com>
2023-08-24 09:55:12 +0800

    Draw the layout of simple list test.

--- app/src/main/java/com/zr/myapplication/ListView/fragment/ListViewCustomFragment.java
+++ app/src/main/java/com/zr/myapplication/ListView/fragment/ListViewCustomFragment.java
@@ -1,5 +1,6 @@
 package com.zr.myapplication.ListView.fragment;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.zr.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,6 +26,10 @@ import com.zr.myapplication.R;
  * create an instance of this fragment.
  */
 public class ListViewCustomFragment extends Fragment {
+
+    private List<Map<String, Object>> mapList;
+
+    private SimpleAdapter simpleAdapter;
 
     public ListViewCustomFragment() {
         // Required empty public constructor
@@ -37,6 +49,16 @@ public class ListViewCustomFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_view_custom, container, false);
         ListView listView = view.findViewById(R.id.custom_list_view_test);
+        mapList = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("img", R.mipmap.ic_launcher);
+            map.put("text",  "这是第 "+(i + 1) +" 条数据");
+            mapList.add(map);
+        }
+        simpleAdapter = new SimpleAdapter(getActivity(), mapList, R.layout.layout_test_simple_list_item, new String[]{"img", "text"}, new int[]{R.id.image_view, R.id.image_text}
+        );
+        listView.setAdapter(simpleAdapter);
         return view;
     }
 }
