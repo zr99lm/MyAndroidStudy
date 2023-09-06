6d5a51ba8556708e373289b451fdb62512450fae
zr99lm <z1064207498@gmail.com>
2023-09-06 17:09:25 +0800

    use lombok for class all constructor.

--- app/src/main/java/com/zr/myapplication/ListView/ItemBean.java
+++ app/src/main/java/com/zr/myapplication/ListView/ItemBean.java
@@ -1,8 +1,12 @@
 package com.zr.myapplication.ListView;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemBean {
     private int img;
     private String title;
